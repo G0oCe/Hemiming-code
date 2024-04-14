ed9c214abc1ae4364c8ab437852702b87d6e0118
G0oCe <alim.sadigov20@gmail.com>
2024-04-14 10:24:13 +0400

    Correct mistake in encoder

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ uint16_t hamming_encode(uint8_t data) {
     uint8_t D3 = (data >> 1) & 1;
     uint8_t D4 = data & 1;
 
-    uint8_t P1 = D1 ^ D3 ^ D4;
-    uint8_t P2 = D1 ^ D2 ^ D4;
-    uint8_t P3 = D1 ^ D2 ^ D3;
-    uint8_t P4 = P1 ^ D1 ^ D2 ^ D3 ^ D4;
+    uint8_t P1 = 1 ^ D1 ^ D3 ^ D4;
+    uint8_t P2 = 1 ^ D1 ^ D2 ^ D4;
+    uint8_t P3 = 1 ^ D1 ^ D2 ^ D3;
+    uint8_t P4 = 1 ^ P1 ^ D1 ^ P2 ^ D2 ^ P3 ^ D3 ^ D4;
 
     return (P1 << 7) | (D1 << 6) | (P2 << 5) | (D2 << 4) | (P3 << 3) | (D3 << 2) | (P4 << 1) | D4;
 }
